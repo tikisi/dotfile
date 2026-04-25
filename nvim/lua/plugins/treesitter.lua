cdbf8ed5da7d8be4737abf2ac68fd91c81046632
tikisi <faohfh@gmail.com>
2026-04-26 07:47:00 +0900

    treesitter対応の言語を追加

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,36 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "go", "gomod", "gosum", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
-      highlight = { enable = true },
-      indent = { enable = true },
+    local parsers = {
+        "go",
+        "gomod",
+        "gosum",
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+        "c",
+        "cpp",
+        "c_sharp",
+        "json",
+        "bash",
+        "python",
+        "javascript",
+        "typescript"
+    }
+
+    require("nvim-treesitter").install(parsers)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "go", "gomod", "gosum", "lua", "vim", "help", "markdown" },
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
